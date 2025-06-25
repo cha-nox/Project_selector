3ad4e7356ac61c82df1854b55f330926e2b3ba89
FrozenCell239 <val8ioks@gmail.com>
2025-06-25 12:52:38 +0200

    Feature addition - Added the new project opening feature. (May be enhanced later in order to create actual project using Symfony, Node.js, etc...) - Minor changes and fixes in main.py file.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from enum                   import StrEnum
 from fnmatch                import fnmatch
 from InquirerPy.inquirer    import select
 from json                   import JSONDecodeError, load
-from os                     import listdir, system, path
+from os                     import listdir, makedirs, system
+from os.path                import isdir, join
 from sys                    import exit as sysExit
+
+class OtherChoices(StrEnum) :
+    """Choices for the menu."""
+    NEW_PROJECT_OPTION  : str = "+ Nouveau"
+    CANCEL_OPTION       : str = "× Annuler"
 
 def loadSettings(settings_file : str = 'settings.json') -> dict[str, str|bool] :
     """Loads settings from settings.json file."""
@@ -14,6 +21,20 @@ def loadSettings(settings_file : str = 'settings.json') -> dict[str, str|bool] :
     except Exception as e       : print(f"Erreur lors du chargement de {settings_file}. : {e}")
     return {}
 
+def loadProjectsList(directory : str, ignored_directories : list[str]) -> list[str] :
+    """Returns a list of available projects folders in the given directory, excluding ignored ones."""
+
+    if not directory or not isdir(directory) : return []
+    try :
+        return sorted(
+            f"• {name}"
+            for name in listdir(directory)
+            if isdir(join(directory, name)) and not checkIgnoredDirectory(name, ignored_directories)
+        )
+    except Exception as e :
+        print(f"Erreur en lisant {directory} : {e}")
+        return []
+
 def checkIgnoredDirectory(directory_name : str, ignored_patterns : list[str]) -> bool :
     """Check if a directory should be ignored based on the ignored patterns."""
     
@@ -21,25 +42,12 @@ def checkIgnoredDirectory(directory_name : str, ignored_patterns : list[str]) ->
         if fnmatch(directory_name.lower(), pattern.lower()) : return True
     return False
 
-def loadProjectsList(directory : str, ignored_directories : list[str]) -> list[str] :
-    """Returns a list of available projects folders in the given directory, excluding ignored ones."""
-
-    if not directory or not path.isdir(directory) : return []
-    try :
-        return sorted(
-            f"• {name}"
-            for name in listdir(directory)
-            if path.isdir(path.join(directory, name)) and not checkIgnoredDirectory(name, ignored_directories)
-        )
-    except Exception as e :
-        print(f"Erreur en lisant {directory} : {e}")
-        return []
-
 if __name__ == '__main__' :
+    project_path        : str                   = ''
+    new_project_name    : str                   = ''
     settings            : dict[str, str|bool]   = loadSettings()
     projects_directory  : str                   = settings.get('projects_directory', '')
     ignored_directories : list[str]             = settings.get('ignored_directories', [])
-    cancel_option       : str                   = "× Annuler"
     options             : list[str]             = loadProjectsList(projects_directory, ignored_directories)
 
     # Clearing the terminal
@@ -52,13 +60,27 @@ if __name__ == '__main__' :
         sysExit(2012)
 
     # Displaying the menu
-    choice = select(
+    choice : select = select(
         message     = "Sélectionnez un projet à ouvrir.",
-        choices     = options + [cancel_option],
+        choices     = options + [OtherChoices.NEW_PROJECT_OPTION, OtherChoices.CANCEL_OPTION],
         pointer     = "→",
         instruction = "Utilisez ↑ ↓ pour naviguer puis pressez Entrée pour ouvrir le projet."
     ).execute()
 
     # Processing user's choice
-    if choice != cancel_option : system(f'code "{projects_directory}/{choice[2:]}"')
+    match choice :
+        case OtherChoices.NEW_PROJECT_OPTION :
+            while new_project_name == '' :
+                new_project_name = input("Nom du projet : ")
+                project_path = join(projects_directory, new_project_name)
+                if new_project_name == '' :
+                    print("Le nom du projet ne peut pas être vide.")
+                elif isdir(project_path) :
+                    print("Ce nom de projet est déjà utilisé.")
+                    new_project_name = ''
+                else : break
+            makedirs(project_path)
+            system(f'code "{project_path}"')
+        case OtherChoices.CANCEL_OPTION : pass
+        case _ : system(f'code "{join(projects_directory, choice[2:])}"')
     sysExit(0)
